4e99d81a8e078520e6eb2a4784a218434fe47b31
Islam Shehata <islam@shehata.io>
2022-12-02 16:16:55 +0200

    use clean_results from local utils

--- api/random.py
+++ api/random.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
+from utils.api import clean_results
 import json
 import arrand.arrandom
 
@@ -19,7 +20,7 @@ class handler(BaseHTTPRequestHandler):
         needed_count = max_length
         while needed_count > 0:
             res = arrand.arrandom.rand_sentences(needed_count)
-            res = self.clean_results(res, "")
+            res = clean_results(res, "")
             t.extend(res)
             needed_count -= len(res)
 
@@ -27,37 +28,3 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','application/json')
         self.end_headers()
         self.wfile.write(json.dumps(t).encode())
-
-    def clean_results(res, category):
-        new_res = []
-        for r in res:
-            if r == '':
-                continue
-            elif len(r) < 30:
-                continue
-            elif '****' in r:
-                continue
-            elif 'a' in r:
-                # skip if its in english
-                continue
-
-            if '</t>' in r:
-                r.replace('</t>', '')
-            elif r[-1] == '/':
-                r = r[:-1]
-
-            # if string starts with number, remove the number
-            if r[:1].isdigit() and category == "poem":
-                r = r[2:]
-
-            print(r[-1])
-
-            r.strip()
-            r.strip('/')
-            r.strip('\/')
-            r.strip('\\')
-            r.strip(' /')
-
-            new_res.append(r)
-
-        return new_res
