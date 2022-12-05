a5061ba730d5da87b395f6499847f70a23442fba
Islam Shehata <islam@shehata.io>
2022-12-05 18:30:58 +0200

    fix paths to icons in head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -19,14 +19,14 @@ export default function Head() {
       <meta name="og:site_name" property="og:site_name" content="مِصباح لتوليد النصوص العشوائية" />
       <meta name="og:description" property="og:description" content="مصباح هو أداة لتوليد النصوص العشوائية باللغة العربية لاستخدامها في قوالب التصميم. يمكنك التحكم في عدد الجمل وعدد الفقرات ونسخها بضغطة زر. يمكن اعتبار مصباح كـ Lorem Ipsum للغة العربية."></meta>
       <meta name="og:url" property="og:url" content="https://mesba7.com"></meta>
-      <meta name="og:image" property="og:image" content="https://mesba7.com/icons/icon_512x512.f18552.png"></meta>
+      <meta name="og:image" property="og:image" content="https://mesba7.com/icons/icon_512x512.png"></meta>
       <meta name="og:image:width" property="og:image:width" content="512"></meta>
       <meta name="og:image:height" property="og:image:height" content="512"></meta>
       <meta name="og:image:type" property="og:image:type" content="image/png"></meta>
 
       <link rel="icon" href="/favicon.ico" />
-      <link rel="shortcut icon" href="/icons/icon_64x64.f18552.png"></link>
-      <link rel="apple-touch-icon" href="/icons/icon_512x512.f18552.png" sizes="512x512"></link>
+      <link rel="shortcut icon" href="/icons/icon_64x64.png"></link>
+      <link rel="apple-touch-icon" href="/icons/icon_512x512.png" sizes="512x512"></link>
       <link rel="manifest" href="/manifest.json"></link>
     </>
   )
